d964015bcbf3350366b3228cbb88cc5b1c0b46ad
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-04 01:48:53 +0200

    improved mobile and text styling
    improvements to mobile styling, improvements to text styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
       <div className="bg-standard flex-center w-full">
         <div className="noisy w-full flex justify-center">
-    <div className="w-full max-w-400 min-h-screen flex-col flex justify-center">
+    <div className="w-full max-w-400 min-h-screen flex-col flex justify-center text-black">
     <Hero/>
     <OmMig/>
       </div>
