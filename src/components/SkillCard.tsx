871843c243ba5c633765b8b098da687a4a3ec489
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-04 01:14:35 +0200

    improved skillcard

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -7,7 +7,7 @@ type SkillCardProps = {
 
 export default function SkillCard({name, icon}: SkillCardProps) {
     return (
-        <div className="flex-center space-x-2 p-4 bg-white/25 rounded-xl shadow-md w-30 h-15">
+        <div className="flex-center space-x-2 p-4 bg-white/25 rounded-xl shadow-md">
             <Icon icon={`${icon}`} width="20"/>
             <p className="text-md font-medium">{name}</p>
         </div>
