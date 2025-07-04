1bacf848ded8c5b02456f89d5a3f60e01eac114a
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-04 05:06:07 +0200

    improved SEO
    now only 1 h1 tag

--- src/components/OmMig.tsx
+++ src/components/OmMig.tsx
@@ -7,18 +7,18 @@ export default function OmMig () {
             <section className="min-h-screen w-full flex-center flex-col">
                 <Line/>
                 <div className="flex-center flex-col">
-                    <h1 className="text-center text-6xl md:text-7xl text-gradient-subtle font-bold h-50">Om mig</h1>
+                    <h2 className="text-center text-6xl md:text-7xl text-gradient-subtle font-bold h-50">Om mig</h2>
                 </div>
                 <div className="flex-col md:flex-row flex-center w-full">
                     <div className="flex items-center px-5 w-full md:w-[40%] flex-col m-5 md:glass-box">
                         <div className="flex items-center flex-col w-full">
-                            <h1 className="text-black/95 md:text-wrap md:text-left text-center text-3xl md:text-4xl font-semibold px-5 md:px-0">HTX&apos;er med flair for hjemmesideudvikling</h1>
+                            <h2 className="text-black/95 md:text-wrap md:text-left text-center text-3xl md:text-4xl font-semibold px-5 md:px-0">HTX&apos;er med flair for hjemmesideudvikling</h2>
                             <div className="m-5"/>
                             <p className="text-black/90 font-thin md:text-left md:text-wrap text-center px-5 md:px-0">Selvstændig og nysgerrig HTX’er med flair for hjemmesideudvikling og et skarpt øje for detaljen. Jeg kombinerer teknisk kunnen med kreativ tænkning og brænder for at skabe brugervenlige, moderne og visuelt indbydende webløsninger – altid med fokus på performance, funktion og æstetik.</p>
                         </div>
                     </div>
                     <div className="md:glass-box px-5 md:px-0 flex items-center flex-col md:w-[40%] m-5">
-                        <h1 className="text-gradient text-4xl text-center font-semibold">Mine skills</h1>
+                        <h2 className="text-gradient text-4xl text-center font-semibold">Mine skills</h2>
                         <div className="m-5"/>
                         <div className="flex-center flex-wrap gap-4">
                             <SkillCard name="Tailwind CSS" icon="devicon:tailwindcss" website="https://tailwindcss.com/"/>
