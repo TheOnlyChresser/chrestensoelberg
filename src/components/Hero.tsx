50667c2dacc64c695a1cfa5ed3d0e0487b3b6fe4
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-05 11:47:23 +0200

    improved hand animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,6 +21,11 @@ const Hero = () => {
             repeat: 11,
             transformOrigin: "130% 130%",
             ease: "power1.inOut",
+            onComplete: () => {gsap.to("#theghost", {
+                opacity: 0,
+                duration: 1,
+                ease: "power2.inOut",
+            })}
         });
         const tl = gsap.timeline({})
         tl.to("#ego", {
@@ -70,7 +75,7 @@ const Hero = () => {
                         </div>
                         <div className="flex-1">
                             <h2
-                                className="hidden md:flex md:mb-10 md:text-7xl font-bold md:flex-row text-black"
+                                id="theghost" className="hidden md:flex md:mb-5 md:text-2xl font-bold md:flex-row text-black/75"
                             ><span className="font-inter mr-5">Hej</span><span id="hand" role="img" aria-label="waving hand">👋🏻</span></h2>
                             <h1 className="text-4xl md:text-7xl mb-10 pb-2 bg-gradient-to-br from-black from-30% via-[#d5d8f6] via-80% to-[#fdf7fe] bg-clip-text font-bold leading-[1] tracking-tight text-transparent">
                                 <span className="md:hidden">Hej, </span>Jeg hedder <span className="text-4xl md:text-7xl text-transparent bg-clip-text bg-gradient-to-r from-[#A3F7BF] to-[#C5C9FF]" id="titlespan">Chresten</span>
