88b0c6bd1fc93228a71d415a3acb3c7aca80aff5
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-07 03:58:54 +0200

    added blog via website template

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        domains: ['localhost'],
+    },
+    // any other config options here
 };
 
 export default withPayload(nextConfig);
