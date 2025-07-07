c76de229dd1a1954b5a78be09e5b55d4955f3f29
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-07 02:49:54 +0200

    Revert "initial"
    This reverts commit 786a62f252069917006d28f502ce28a19a04f589.

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
-import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3001',
-        pathname: '/api/media/file/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/api/media/file/**',
-      },
-    ],
-  },
+  /* config options here */
 };
 
-export default withPayload(nextConfig);
+export default nextConfig;
