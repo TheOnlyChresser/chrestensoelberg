574645ce8aac913e4748fb5bed62f429b17bc522
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-07 20:34:09 +0200

    .

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ["localhost"],
+        domains: ["chrestensoelberg.dk"],
     },
     eslint: {
         ignoreDuringBuilds: true,
