10f5ed3c6bdc4b0e8f28eb9f3d0d4f6d1ade5765
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-08 03:38:14 +0200

    oopsi again... test 3 :D

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -53,7 +53,7 @@ export default function SkillCard({name, icon, website, description}: SkillCardP
         <>
             <div 
                 onClick={handleClick}
-                className="cursor-pointer flex-center space-x-2 p-4 bg-white/25 rounded-xl shadow-md hover:shadow-lg active:shadow-lg group hover:scale-110 focus:scale-110 transition-all duration-200"
+                className="cursor-pointer flex-center space-x-2 p-4 bg-white/25 rounded-xl shadow-md hover:shadow-lg focus:shadow-lg group hover:scale-110 focus:scale-110 transition-all duration-200"
             >
                 <Icon icon={`${icon}`} width="24"/>
                 <p className="text-md font-medium group-hover:text-lg">{name}</p>
