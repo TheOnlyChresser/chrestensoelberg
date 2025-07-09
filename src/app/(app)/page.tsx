7e46626b868fa1624849a8106df092191b9423fa
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-09 20:19:17 +0200

    updated image styling

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -39,8 +39,8 @@ export const metadata = {
     images: [
       {
         url: "/avatar.png",
-        width: 1200,
-        height: 630,
+        width: 1000,
+        height: 1000,
         alt: "Chresten Soelberg - Frontend Udvikler",
       },
     ],
