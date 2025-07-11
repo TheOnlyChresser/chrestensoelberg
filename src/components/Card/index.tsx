dbd92000d11de972d66bae06fc55530fef095a13
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-07-11 20:41:52 +0200

    updated blog

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -32,48 +32,43 @@ export const Card: React.FC<{
   return (
     <article
       className={cn(
-        'glass-box hover:cursor-pointer hover:bg-black/30 hover:border-black/80 hover:text-white active:bg-black/30 active:border-black/80 active:text-white overflow-hidden',
+        'group glass-box hover:cursor-pointer hover:bg-black/30 hover:border-black/80 hover:text-white active:bg-black/30 active:border-black/80 active:text-white overflow-hidden',
         className,
       )}
       ref={card.ref}
     >
       <div className="p-4">
-        {showCategories && hasCategories && (
-          <div className="uppercase text-sm mb-4 italic">
-            {showCategories && hasCategories && (
-              <div>
-                {categories?.map((category, index) => {
-                  if (typeof category === 'object') {
-                    const { title: titleFromCategory } = category
-
-                    const categoryTitle = titleFromCategory || 'Untitled category'
-
-                    const isLast = index === categories.length - 1
-
-                    return (
-                      <Fragment key={index}>
-                        {categoryTitle}
-                        {!isLast && <Fragment>, &nbsp;</Fragment>}
-                      </Fragment>
-                    )
-                  }
-
-                  return null
-                })}
-              </div>
-            )}
-          </div>
-        )}
         {titleToUse && (
           <div className="prose font-bold">
             <h3>
-              <Link className="not-prose" href={href} ref={link.ref}>
+              <Link className="not-prose text-black group-hover:text-white" href={href} ref={link.ref}>
                 {titleToUse}
               </Link>
             </h3>
           </div>
         )}
-        {description && <div className="mt-2">{description && <p>{sanitizedDescription}</p>}</div>}
+        {showCategories && hasCategories && (
+            <div className="uppercase text-sm mt-4 flex items-center italic">
+              <div className="flex flex-wrap gap-2 mb-4">
+                {categories?.map((category, index) => {
+                  if (typeof category === 'object') {
+                    const { title: titleFromCategory } = category
+                    const categoryTitle = titleFromCategory || 'Untitled category'
+
+                    return (
+                        <span
+                            key={index}
+                            className="bg-blue-500/10 group-hover:bg-white/10 border border-black/20 rounded-full px-3 py-1 text-xs uppercase italic"
+                        >
+                        {categoryTitle}
+                      </span>
+                    )}
+                  return null
+                })}
+              </div>
+            </div>
+        )}
+        {description && <div className="mt-1">{description && <p>{sanitizedDescription}</p>}</div>}
       </div>
     </article>
   )
