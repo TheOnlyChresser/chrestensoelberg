1024751ed914ea01c1aaa899fe3ebe9d41106537
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-10-13 18:18:13 +0200

    improved styling

--- src/app/(app)/noter/page.tsx
+++ src/app/(app)/noter/page.tsx
@@ -29,7 +29,7 @@ export default async function Page() {
 
   return (
     <div className="bg-gradient-to-tr from-white via-blue-50 to-green-50  dark:from-black dark:via-[#050A1F] dark:to-[#041F12] flex-center w-full">
-    <div className="min-h-screen w-full mb-20 md:mb-4">
+    <div className="min-h-screen w-full mb-20 md:mb-4 flex flex-col items-center">
       <PageClient />
       <div className="mt-10 mb-10 flex-center text-8xl text-gradient font-bold h-30">
         <div className="prose dark:prose-invert max-w-none">
