d24ed167fc21edf3288dc6a8b2e5849902b02e30
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-10-13 17:52:24 +0200

    improved mobile styling

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -4,7 +4,7 @@ export default function Quote() {
     return (
         <section className="max-w-[1000px] px-4 md:px-40 pt-16 md:pt-0 pb-32 md:pb-40 flex flex-col items-center text-center">
             <div className="relative">
-                <QuoteIcon className="absolute -top-8 -left-6 text-gray-300 w-12 h-12" />
+                <QuoteIcon className="absolute -top-8 md:-left-6 text-gray-300 w-12 h-12" />
                 <blockquote className="text-3xl md:text-4xl font-serif italic text-black/80 leading-relaxed">
                     “Videns største fjende er ikke uvidenhed, det er illusionen om viden.”
                 </blockquote>
