f054ed6145cf40fc0b8c84d4c2ceed4d849dca82
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-10-14 22:45:25 +0200

    fixed error

--- src/components/blog-animations.tsx
+++ src/components/blog-animations.tsx
@@ -13,119 +13,117 @@ export default function BlogAnimations() {
         const h5element = document.querySelectorAll(".prose h5")
         const olelement = document.querySelectorAll(".prose ol")
         const blockselement = document.querySelectorAll(".blocks")
-        document.fonts.ready.then(() => {
-            h1element.forEach((h1) => {
+        h1element.forEach((h1) => {
 
-                gsap.from(h1, {
-                    duration: 2,
-                    ease: "expo.out",
-                    y: -40,
-                    opacity: 0,
-                    scrollTrigger: {
-                        trigger: h1,
-                        start: "top bottom"
-                    },
-                    stagger: 0.01,
-                })
+            gsap.from(h1, {
+                duration: 2,
+                ease: "expo.out",
+                y: -40,
+                opacity: 0,
+                scrollTrigger: {
+                    trigger: h1,
+                    start: "top bottom"
+                },
+                stagger: 0.01,
             })
-            h2element.forEach((h2) => {
+        })
+        h2element.forEach((h2) => {
 
-                gsap.from(h2, {
-                    duration: 2,
-                    ease: "expo.out",
-                    y: -10,
-                    opacity: 0,
-                    scrollTrigger: {
-                        trigger: h2,
-                        start: "top bottom"
-                    },
-                    stagger: 0.02,
-                })
+            gsap.from(h2, {
+                duration: 2,
+                ease: "expo.out",
+                y: -10,
+                opacity: 0,
+                scrollTrigger: {
+                    trigger: h2,
+                    start: "top bottom"
+                },
+                stagger: 0.02,
             })
-            h3element.forEach((h3) => {
+        })
+        h3element.forEach((h3) => {
 
-                gsap.from(h3, {
-                    duration: 2,
-                    ease: "expo.out",
-                    y: -10,
-                    opacity: 0,
-                    scrollTrigger: {
-                        trigger: h3,
-                        start: "top bottom"
-                    },
-                    stagger: 0.02,
-                })
+            gsap.from(h3, {
+                duration: 2,
+                ease: "expo.out",
+                y: -10,
+                opacity: 0,
+                scrollTrigger: {
+                    trigger: h3,
+                    start: "top bottom"
+                },
+                stagger: 0.02,
             })
-            h4element.forEach((h4) => {
+        })
+        h4element.forEach((h4) => {
 
-                gsap.from(h4, {
-                    duration: 2,
-                    ease: "expo.out",
-                    y: -10,
-                    opacity: 0,
-                    scrollTrigger: {
-                        trigger: h4,
-                        start: "top bottom"
-                    },
-                    stagger: 0.02,
-                })
+            gsap.from(h4, {
+                duration: 2,
+                ease: "expo.out",
+                y: -10,
+                opacity: 0,
+                scrollTrigger: {
+                    trigger: h4,
+                    start: "top bottom"
+                },
+                stagger: 0.02,
             })
-            h5element.forEach((h5) => {
+        })
+        h5element.forEach((h5) => {
 
-                gsap.from(h5, {
-                    duration: 2,
-                    ease: "expo.out",
-                    y: -10,
-                    opacity: 0,
-                    scrollTrigger: {
-                        trigger: h5,
-                        start: "top bottom"
-                    },
-                    stagger: 0.02,
-                })
+            gsap.from(h5, {
+                duration: 2,
+                ease: "expo.out",
+                y: -10,
+                opacity: 0,
+                scrollTrigger: {
+                    trigger: h5,
+                    start: "top bottom"
+                },
+                stagger: 0.02,
             })
-            pelement.forEach((p) => {
+        })
+        pelement.forEach((p) => {
 
-                gsap.from(p, {
-                    delay: 0.2,
-                    duration: 1,
-                    ease: "expo.out",
-                    xPercent: -2,
-                    autoAlpha: 0,
-                    scrollTrigger: {
-                        trigger: p,
-                        start: "top bottom"
-                    },
-                    stagger: 0.1,
-                })
+            gsap.from(p, {
+                delay: 0.2,
+                duration: 1,
+                ease: "expo.out",
+                xPercent: -2,
+                autoAlpha: 0,
+                scrollTrigger: {
+                    trigger: p,
+                    start: "top bottom"
+                },
+                stagger: 0.1,
             })
-            olelement.forEach((ol) => {
+        })
+        olelement.forEach((ol) => {
 
-                gsap.from(ol, {
-                    duration: 1,
-                    ease: "expo.out",
-                    xPercent: -4,
-                    autoAlpha: 0,
-                    scrollTrigger: {
-                        trigger: ol,
-                        start: "top bottom"
-                    },
-                    stagger: 0.01,
-                })
+            gsap.from(ol, {
+                duration: 1,
+                ease: "expo.out",
+                xPercent: -4,
+                autoAlpha: 0,
+                scrollTrigger: {
+                    trigger: ol,
+                    start: "top bottom"
+                },
+                stagger: 0.01,
             })
-            blockselement.forEach((block) => {
+        })
+        blockselement.forEach((block) => {
 
-                gsap.from(block, {
-                    duration: 1,
-                    ease: "expo.out",
-                    xPercent: -4,
-                    autoAlpha: 0,
-                    scrollTrigger: {
-                        trigger: block,
-                        start: "top bottom"
-                    },
-                    stagger: 0.1,
-                })
+            gsap.from(block, {
+                duration: 1,
+                ease: "expo.out",
+                xPercent: -4,
+                autoAlpha: 0,
+                scrollTrigger: {
+                    trigger: block,
+                    start: "top bottom"
+                },
+                stagger: 0.1,
             })
         })
     })
