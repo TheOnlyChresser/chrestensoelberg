71131f7fedc35ad50fc52d206887d31c06a4d3cb
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-10-14 03:04:36 +0200

    improved styling

--- src/components/Kontakt.tsx
+++ src/components/Kontakt.tsx
@@ -134,7 +134,7 @@ function Kontakt() {
                         id="message"
                         name="message"
                         rows={5}
-                        className="bg-blue-500/2 backdrop-blur-sm border border-black/30 text-black/80 placeholder-black/60 rounded-md px-4 py-2 w-full focus:outline-none focus:ring-1 focus:ring-black/80"
+                        className="resize-none bg-blue-500/2 backdrop-blur-sm border border-black/30 text-black/80 placeholder-black/60 rounded-md px-4 py-2 w-full focus:outline-none focus:ring-1 focus:ring-black/80"
                         value={userInput.message}
                         onChange={handleChange}
                         required
