e79b01a7f35c1fc6df7e789da1853b02f6ded058
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-10-15 17:39:19 +0200

    added colors from payload documentation

--- src/collections/Posts/index.ts
+++ src/collections/Posts/index.ts
@@ -11,7 +11,7 @@ import {
   OrderedListFeature,
   UnorderedListFeature,
   ParagraphFeature,
-  BlockquoteFeature, TextStateFeature,
+  BlockquoteFeature, TextStateFeature, defaultColors,
 } from '@payloadcms/richtext-lexical'
 
 import { authenticated } from '../../access/authenticated'
@@ -107,7 +107,25 @@ export const Posts: CollectionConfig<'posts'> = {
                     OrderedListFeature(),
                     UnorderedListFeature(),
                     BlockquoteFeature(),
-                    TextStateFeature(),
+                    TextStateFeature({
+                      // prettier-ignore
+                      state: {
+                        color: {
+                          ...defaultColors,
+                          // fancy gradients!
+                          galaxy: { label: 'Galaxy', css: { background: 'linear-gradient(to right, #0000ff, #ff0000)', color: 'white' } },
+                          sunset: { label: 'Sunset', css: { background: 'linear-gradient(to top, #ff5f6d, #6a3093)' } },
+                        },
+                        // You can have both colored and underlined text at the same time.
+                        // If you don't want that, you should group them within the same key.
+                        // (just like I did with defaultColors and my fancy gradients)
+                        underline: {
+                          'solid': { label: 'Solid', css: { 'text-decoration': 'underline', 'text-underline-offset': '4px' } },
+                          // You'll probably want to use the CSS light-dark() utility.
+                          'yellow-dashed': { label: 'Yellow Dashed', css: { 'text-decoration': 'underline dashed', 'text-decoration-color': 'light-dark(#EAB308,yellow)', 'text-underline-offset': '4px' } },
+                        },
+                      },
+                    }),
                   ]
                 },
               }),
