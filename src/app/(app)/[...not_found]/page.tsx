f2befb1ba277083d487d2e251acc8159b9ab0ed0
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-11-02 20:59:43 +0100

    improved styling

--- src/app/(app)/[...not_found]/page.tsx
+++ src/app/(app)/[...not_found]/page.tsx
@@ -4,8 +4,10 @@ import {Heading, Subheading} from "@/components/ChresserComponents/ui/Text";
 export default function d() {
     return (
         <div className="w-full min-h-screen flex items-center justify-center flex-col">
-            <Heading>404</Heading>
-            <Subheading className="!mt-2">Siden kunne ikke blive fundet.</Subheading>
+            <div className="bg-white flex flex-col items-center justify-center p-4 rounded-2xl shadow-md">
+                <Heading className="!mt-0">404</Heading>
+                <Subheading className="!mt-2">Siden kunne ikke blive fundet.</Subheading>
+            </div>
             <Button className="mt-8" button="normal">Gå tilbage til forsiden</Button>
         </div>
     )
