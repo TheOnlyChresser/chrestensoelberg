410678a80611a2f13d49e08017a67eece9b462ec
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-11-02 15:59:19 +0100

    fixed error

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ChresserComponents/navBar";
 import { SpeedInsights } from '@vercel/speed-insights/next';
