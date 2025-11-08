6789dd582617bb6781cccf98404c4abb3a8becd9
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-11-08 21:33:02 +0100

    fixed error

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export const getValidSubdomain = (host?: string | null) => {
         // On client side, get the host from window
         host = window.location.host;
     }
-    if (host && host.includes('.')) {
+    if (host && host.includes('.') && host !== 'chrestensoelberg.dk' && host !== 'www.chrestensoelberg.dk') {
         const candidate = host.split('.')[0];
         if (candidate && !candidate.includes('localhost')) {
             // Valid candidate
