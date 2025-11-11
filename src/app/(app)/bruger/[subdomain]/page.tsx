982e730b820fac0aaa1748ef1693afe0b49d1cb2
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-11-11 20:28:18 +0100

    added design

--- src/app/(app)/bruger/[subdomain]/page.tsx
+++ src/app/(app)/bruger/[subdomain]/page.tsx
@@ -47,6 +47,7 @@ export default function Page() {
                                 <AttentionSpan color="blue" className="!animate-none !from-blue-100 !via-blue-100 !to-blue-100 !cursor-default" title="Status">{order.status}</AttentionSpan>
                             </div>
                         </div>
+                        <div className="flex items-center">
                         <div className="grid md:grid-cols-3 gap-4 p-4 mx-4 md:mx-0 pb-24 md:pb-0">
                             <div className="shadow-xs md:col-span-2 w-full p-4 pb-6 border bg-white max-h-[80vh] md:max-h-[60vh] overflow-y-hidden">
                                 <span>Beskeder</span>
@@ -80,6 +81,14 @@ export default function Page() {
                                         )}
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                            <div className="flex w-full mx-8 items-center justify-center rounded-xl border bg-white">
+                                {order.design !== "" ? (
+                                    <img src={order.design} alt="billede af design" className="w-full h-full object-cover"/>
+                                ): (
+                                    <p className="text-center text-sm text-gray-500">Designet er ikke færdigt endnu.</p>
+                                )}
                             </div>
                         </div>
                     </div>
